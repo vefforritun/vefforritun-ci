5862221a845c6e2bd015c3384b4d8c2c286f2e07
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-04-05 16:57:42 +0000

    wip imaginary

--- test/tests.js
+++ test/tests.js
@@ -2,12 +2,17 @@ const chai = require('chai');
 const quadratic = require('../quadratic');
 
 chai.should();
+chai.expect();
 
 describe('Quadratic', () => {
   it('should solve a basic case', () => {
     const result = quadratic(3, 6, -105);
     result.x1.should.equal(45);
     result.x2.should.equal(-63);
+  });
+
+  it('should throw an error for imaginary roots', () => {
+    quadratic(1, 1, 1).to.throw();
   });
 });
 
